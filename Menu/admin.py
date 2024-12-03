a8a9aa5abc7ec0741643c68cbe6efce86868e2e3
alik <qurbonovalijon30@gmail.com>
2024-12-03 16:04:08 +0500

    Added  admin.py

--- Menu/admin.py
+++ Menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Borrow)
+admin.site.register(Review)
+admin.site.register(Genre)
